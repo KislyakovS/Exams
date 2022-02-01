547fde119bda6d0a23ad68c74e0e023fa60b91b9
Alexandr Kislyakov <kislyakov.sk@yandex.ru>
2022-02-01 21:41:22 +0300

    fix between

--- Tasks/between.js
+++ Tasks/between.js
@@ -1,21 +1,30 @@
 // Extract substring between prefix and suffix
 
 getvaluebetween = (str, p, s) => {
-  i = str.indexOf(p);
-  if (i === -1) return '';
-  else {
-    k = i + p.length;
-    str = str.substring(k);
-    if (s) {
-      i = str.indexOf(s);
-      if (i === -1) {
-        return '';
-      } else {
-        str = str.substring(0, i);
-      }
-    }
-  }
-  return str;
+  // OLD
+  // i = str.indexOf(p);
+  // if (i === -1) return '';
+  // else {
+  //   k = i + p.length;
+  //   str = str.substring(k);
+  //   if (s) {
+  //     i = str.indexOf(s);
+  //     if (i === -1) {
+  //       return '';
+  //     } else {
+  //       str = str.substring(0, i);
+  //     }
+  //   }
+  // }
+  // return str;
+
+  // NEW
+  if (str.trim() === '') return '';
+
+  const startIndex = str.indexOf(p) + 1;
+  const endIndex = str.indexOf(s);
+
+  return str.slice(startIndex, endIndex);
 };
 
 require('../Tests/between.js')(getvaluebetween);
